d144dbba90ca95afeb522af55f788de7f4cbeae6
Adam Jacobs <ajman0390@gmail.com>
2019-09-29 20:55:31 -0400

    Edit functionality

--- client/src/app/providers/auth.service.ts
+++ client/src/app/providers/auth.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject, of} from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
-// ~ WIP ~
-// TODO: authService to auth login/is_Admin for nav links
 export class AuthService {
   is_Authenticated: boolean = false;
   is_Admin: boolean = false;
-  // isAuth: boolean = false;
 
   setAuth(isAuthenticated: boolean): void {
     this.is_Authenticated = isAuthenticated;
@@ -26,5 +20,11 @@ export class AuthService {
   getAdmin(): boolean {
     return this.is_Admin;
   }
+  // setID(isAdmin: boolean): void {
+  //   this.is_Admin = isAdmin;
+  // }
+  // getID(): boolean {
+  //   return this.is_Admin;
+  // }
 
 }
--- client/src/app/teams/teams.component.ts
+++ client/src/app/teams/teams.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router , ActivatedRoute} from '@angular/router';
 import { LeagueService } from '../providers/leagues.service';
 import { TeamService } from '../providers/teams.service';
+import { AuthService } from './../providers/auth.service';
 
 @Component({
   selector: 'app-teams',
@@ -14,14 +15,32 @@ export class TeamsComponent implements OnInit {
   // Array to hold Teams Objects
   teams: Array<string> = [];
 
+  sub: any;
+  user: any = {};
+  ID: number = 0;
+  userName: string = '';
+
   constructor(
     private router: Router, 
+    private route: ActivatedRoute,
     private leagueService: LeagueService, 
-    private teamService: TeamService
+    private teamService: TeamService,
+    private authService: AuthService
     ) {}
 
   ngOnInit() {
-   console.log(sessionStorage.getItem("userID"));
+    if (!(this.authService.getAuth())) {
+      this.router.navigate(['login']);
+    }
+
+    // get userId from Query Params
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        this.ID = params['userId'];
+        // this.userName = params['userId'];
+      })
+   
     // call getLeagues() method in Leagues Service
     this.leagueService.getLeagues().subscribe(data => {
       this.leagues = data;
